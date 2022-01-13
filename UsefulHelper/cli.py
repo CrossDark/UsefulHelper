7ae99eb2b9eb7693a9f49e1431c97cfcec960702
clever <liuhanbo333@icloud.com>
2022-01-13 22:54:44 +0800

    Cli refresh

--- UsefulHelper/cli.py
+++ UsefulHelper/cli.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3.9
 import click
-from UsefulHelper import Start
+# from UsefulHelper import Start
 
 key_list = ['start']
 
 
 @click.command()
 @click.option(
-    "--key", prompt="Method",
+    "--things", prompt="[UsefulHelper]>>",
     help="NONE."
 )
-@click.option(
-    '--value', prompt='Properties',
-    help='__'
-)
-def begin(key, value):
-    print(key, value)
-    try:
-        key(value)
-    except TypeError:
+def main(things):
+    if things == 'start':
         print(0)
     else:
-        print(1)
-
-
-def start(name, path):
-    try:
-        Start(name, path)
-    except IsADirectoryError:
-        pass
+        things = None
+        main(things)
 
 
 if __name__ != '__main__':
-    begin()
-else:
-    print('Error')
+    main()
