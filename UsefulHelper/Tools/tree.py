392d6037f0d3b5e3938929a72b30f352b4e0e27d
clever <liuhanbo333@icloud.com>
2022-01-18 18:03:52 +0800

    manager.py Build Tree init down

--- UsefulHelper/Tools/tree.py
+++ UsefulHelper/Tools/tree.py
@@ -20,31 +20,39 @@ class Build:
         super(Build, self).__init__()
         self.Leval = 0
         self.Describe = []
+        doing = []
+        final = []
         with open('./try/Describe/grammar.usg') as describe:
             describe_line = describe.readlines()
         for i in describe_line:
             level = re.search('[^ ]', i).span()[0] // 4
             if re.findall('~(.+?)~', i):
                 continue
-            self.Describe.append([i.replace('\n', '').replace(' ', ''), level])
+            elif level > self.Leval:
+                pre = doing[-1][0:2]
+                doing[-1] = pre + [True]
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
+                self.Leval = level
+            elif level < self.Leval:
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
+                final += doing
+                doing = []
+                self.Leval = 0
+            elif level == self.Leval:
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
+            else:
+                raise TabError
+            print(self.Leval)
+        self.Describe = final
 
-    def build(self, data=None, level=0):
+    def build(self, father='~', data=None):
         doing = []
         final = {}
-        son = False
         if data is None:
             data = self.Describe
         """Iter"""
         for datum in data:
-            if datum[1] < level:
+            if datum[2]:
                 break
-            elif datum[1] > level:
-                son = True
-                doing.append([datum[0], datum[1] - 1])
-            elif datum[1] // 4 == level:
-                final[datum[0]] = doing
             else:
                 raise TabError
-        if son:
-            final[''] = self.build(doing)
-        return final
