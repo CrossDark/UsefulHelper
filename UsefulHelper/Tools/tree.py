fb42e92c471421c173398d6409bd79ac168d5b94
clever <liuhanbo333@icloud.com>
2022-01-19 17:50:58 +0800

    manager.py Build Tree Node down

--- UsefulHelper/Tools/tree.py
+++ UsefulHelper/Tools/tree.py
@@ -5,9 +5,25 @@ Grammar Tree
 import re
 
 
-class Build:
+class Node:
+    def __init__(self, name='~'):
+        super(Node, self).__init__()
+        self.Name = name
+        self.Tree = {}
+
+    def add(self, name, things):
+        self.Tree[name] = things
+
+    def __repr__(self):
+        return {self.Name: self.Tree}
+
+    def __str__(self):
+        return str({self.Name: self.Tree})
+
+
+class Tree:
     def __init__(self):
-        super(Build, self).__init__()
+        super(Tree, self).__init__()
         self.state = None
         self.Leval = 0
         self.Describe = []
@@ -21,45 +37,47 @@ class Build:
                 continue
             elif level > self.Leval:
                 pre = doing[-1][0]
-                doing[-1] = [pre] + [level]
-                doing.append([i.replace('\n', '').replace(' ', ''), level])
+                doing[-1] = [pre] + [level, True]
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
                 self.Leval = level
             elif level < self.Leval:
-                doing.append([i.replace('\n', '').replace(' ', ''), level])
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
                 final += doing
                 doing = []
                 self.Leval = 0
             elif level == self.Leval:
-                doing.append([i.replace('\n', '').replace(' ', ''), level])
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
             else:
                 raise TabError
         self.Describe = final
 
-    def split(self, data=None):
+    def split(self, data=None, leveled=0):
         if data is None:
             data = self.Describe
         self.state = None
-        self.Leval = 0
-        self.Describe = []
         doing = []
         final = []
+        son = False
         for i in data:
             level = i[1]
             print(doing)
-            if level > self.Leval:
-                # pre = doing[-1][0]
-                # doing[-1] = [pre] + [level]
-                doing.append([i[0], level - 1])
-                self.Leval = level
-            elif level < self.Leval:
-                doing.append([i[0], level - 1])
-                final.append(doing)
-                doing = []
-                self.Leval = 0
-            elif level == self.Leval:
-                doing.append([i[0], level - 1])
+            print(final)
+            print('--------------------------')
+            if i[2]:
+                continue
+            if level > leveled:
+                doing.append([i[0:2] + [True]])
+                leveled = level
+            elif level < leveled:
+                doing.append([i[0:2] + [False]])
+                son = True
+            elif level == leveled:
+                doing.append([i[0:2] + [True]])
             else:
                 raise TabError
+            if son:
+                final.append(self.split(doing, leveled + 1))
+                doing = []
         return final
 
     def tree(self, data=None):
